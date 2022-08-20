e90685417ca720e8146e8ba62dfcd6f69ff97470
+ <+>
2022-08-20 08:39:59 -0400

    math lab

--- answer.js
+++ answer.js
@@ -1,8 +1,9 @@
 function add() {
 
 }
-function subtract() {
-     
+
+function subtract(){
+
 }
 function multiply() {
 
@@ -10,34 +11,31 @@ function multiply() {
 function divide() {
 
 }
-function add() {
-    return "something";
-}
 function add(a, b) {
-    return a + b;
+    return(a + b)
 }
 function subtract(a, b) {
-    return a - b;
+    return(a - b)
 }
 function multiply(a, b) {
-    return a * b;
+    return(a * b)
+
 }
 function divide(a, b) {
-    return a / b;
+    return(a / b)
 }
 function increment(n) {
-    return n + 1;
+    return(n + 1)
 }
 function decrement(n) {
-    return n - 1;
-}
-function makeInt(n,){
-    return parseInt (n);
+    return(n - 1)
 }
 function makeInt(n) {
-    return parseFloat(n);
+    return parseInt(n)
+}
+function makeInt(n) {
+    return parseFloat(n)
 }
 function preserveDecimal(n) {
-    return parseFloat(n);
+    return parseFloat(n)
 }
-  
